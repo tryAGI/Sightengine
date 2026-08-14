1d6ac6e24aba580402b70321da3c09a09f2d77f7
Konstantin S. <havendv@gmail.com>
2026-08-14 19:58:09 +0400

    feat: Updated OpenAPI spec (#43)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Sightengine/Generated/Sightengine.SightengineClient.g.cs
+++ src/libs/Sightengine/Generated/Sightengine.SightengineClient.g.cs
@@ -37,10 +37,17 @@ namespace Sightengine
 
         /// <inheritdoc/>
         public global::Sightengine.AutoSDKClientOptions Options { get; }
+
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Sightengine.SourceGenerationContext.Default);
+
         /// <summary>
         /// 
         /// </summary>
-        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext { get; set; } = global::Sightengine.SourceGenerationContext.Default;
+        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext
+        {
+            get => JsonSerializerContextProvider.Value;
+            set => JsonSerializerContextProvider = new(() => value);
+        }
 
 
         /// <summary>
