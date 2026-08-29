0499f96c48abaca667d871e99311f88ab133897a
Konstantin S. <havendv@gmail.com>
2026-08-29 13:56:01 +0400

    feat: Updated OpenAPI spec (#51)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Sightengine/Generated/Sightengine.JsonSerializerContext.g.cs
+++ src/libs/Sightengine/Generated/Sightengine.JsonSerializerContext.g.cs
@@ -24,6 +24,7 @@ namespace Sightengine
             typeof(global::Sightengine.JsonConverters.UnixTimestampJsonConverter),
         })]
     [global::System.Text.Json.Serialization.JsonSerializable(typeof(global::Sightengine.JsonSerializerContextTypes))]
+    [global::System.Text.Json.Serialization.JsonSerializable(typeof(global::System.Collections.Generic.List<object>), TypeInfoPropertyName = "SystemCollectionsGeneric_ObjectList")]
     [global::System.Text.Json.Serialization.JsonSerializable(typeof(global::Sightengine.RequestInfo))]
     [global::System.Text.Json.Serialization.JsonSerializable(typeof(string))]
     [global::System.Text.Json.Serialization.JsonSerializable(typeof(double))]
